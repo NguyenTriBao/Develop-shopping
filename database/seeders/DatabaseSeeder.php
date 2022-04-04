89f9ccd1a851babf7865d9e4d8cf0c5112a6b44e
Student <Student@tdc.edu.vn>
2022-04-04 16:47:14 +0700

    tuan6

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        DB::table('protypes')->insert([
+            ['type_name' => 'Áo']
+        ]);
     }
 }
