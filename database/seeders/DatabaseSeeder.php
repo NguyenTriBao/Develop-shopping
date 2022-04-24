b79439f3b322bb975188195c8b7f566352d7dae5
NguyenTriBao <86540179+NguyenTriBao@users.noreply.github.com>
2022-04-24 22:59:19 +0700

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,13 +29,60 @@ class DatabaseSeeder extends Seeder
             ['manu_name' => 'Chanel']
         ]);
         DB::table('products')->insert([
+            ['name' => 'Wool double-breasted jacket',
+            'image' => 'Wooldouble-breastedjacket.img',
+            'price' => '94166750',
+            'manu_id' => '1',
+            'type_id' => '1',
+            'quantity' => '1',
+            'description' => 'áo limited'],
+
+
+            ['name' => 'Knit striped cotton jumper',
+            'image' => 'Knitstripedcottonjumper.img',
+            'price' => '33302875',
+            'manu_id' => '1',
+            'type_id' => '1',
+            'quantity' => '2',
+            'description' => 'Áo Knit striped cotton jumper'],
+
+
+            ['name' => 'BELTED PARKA',
+            'image' => 'BELTEDPARKA.img',
+            'price' => '98000000',
+            'manu_id' => '1',
+            'type_id' => '1',
+            'quantity' => '2',
+            'description' => 'áo BELTED PARKA nổi tiếng'],
+
+
+            ['name' => 'SLENDER WALLET',
+            'image' => 'SLENDERWALLET',
+            'price' => '11024400',
+            'manu_id' => '2',
+            'type_id' => '3',
+            'quantity' => '5',
+            'description' => 'Ví VL'],
+
+
+            ['name' => 'TWIST BELT CHAIN WALLET',
+            'image' => 'TWISTBELTCHAINWALLET.img',
+            'price' => '45016300',
+            'manu_id' => '2',
+            'type_id' => '3',
+            'quantity' => '1',
+            'description' => 'Ví limited'],
+
+
             ['name' => 'Áo Polo',
             'image' => 'Aopolo.img',
             'price' => '100000',
             'manu_id' => '1',
             'type_id' => '1',
             'quantity' => '2',
-            'description' => 'Áo polo rất đẹp']
+            'description' => 'Áo polo rất đẹp'],
+
         ]);
+
     }
 }
